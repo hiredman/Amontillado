7d490faa005aead318903141a480580c85e28db3
Kevin Downey (hiredman) <redchin@gmail.com>
2010-06-28 19:24:00 -0700

    WORKS

--- src/amontillado/core.clj
+++ src/amontillado/core.clj
@@ -1,56 +1,77 @@
 (ns amontillado.core
-  (:refer-clojure :exclude [get])
+  (:refer-clojure :excludes [read])
   (:import [java.util UUID]
            [java.io File RandomAccessFile]
            [java.util.concurrent ConcurrentHashMap]
-           [clojure.lang ILookup Associative]))
+           [clojure.lang ILookup Associative PersistentQueue]
+           [java.nio ByteBuffer]))
 
 (def dir (or (System/getProperty "cask.dir") "/tmp/amontillado"))
 
-(def port (or (System/getProperty "cask.port") "7777"))
+(.mkdirs (File. dir))
 
-(def cask-id nil)
+(def casks (ref (PersistentQueue/EMPTY)))
 
-(defn cask-file [uuid]
-  (let [dir (if (.endsWith dir "/")
-              dir
-              (str dir "/"))]
-    (.getChannel (RandomAccessFile. (File. (str dir uuid))))))
+(def size (* 1024 1024 200))
 
-(def cask′ nil)
+(def closeable (agent nil))
 
-(defn new-cask-file []
-  (locking cask-id
-    (locking cask′
-      (alter-var-root cask-id #(do % (UUID/randomUUID)))
-      (alter-var-root cask′ #(do % (cask-file cask-id))))))
+(def open-casks 5)
 
-(def keys (ConcurrentHashMap.))
+(defrecord Cask [name file channel])
+
+(defrecord Vtable [file-name pos size])
+
+(defn new-cask []
+  (let [file-name (format "%s/%s.cask" dir (UUID/randomUUID))
+        file-raf (RandomAccessFile. file-name "rw")
+        file-channel (.getChannel file-raf)]
+    (Cask. file-name file-raf file-channel)))
+
+(defn aquire []
+  (let [cask (dosync
+              (let [head (peek @casks)]
+                (alter casks pop)
+                head))]
+    (or cask (new-cask))))
+
+(defn release [cask]
+  (if (> size (.length (:file cask)))
+    (dosync
+     (alter casks conj cask))
+    (send-off closeable conj cask)))
+
+(defn close-casks [casks]
+  (Thread/sleep (* 1000 30))
+  (doseq [cask casks]
+    (.close (:channel cask))
+    (.close (:file cask))))
 
 (defn write [bytebuffer]
-  (let [[cask cask-id] (locking cask′ [cask′ cask-id])
-        start (.size cask)
-        size (.capacity bytebuffer)]
-    (locking cask
-      (.position cask (+ start size)))
-    (.write cask bytebuffer start)
-    [start size cask-id]))
+  (let [cask (aquire)
+        fc (:channel cask)
+        start (.position fc)]
+    (try
+     (.position fc (+ start (.capacity bytebuffer)))
+     (finally
+      (release cask)))
+    (.write fc bytebuffer start)
+    (send-off closeable close-casks)
+    (Vtable. (:name cask) start (.capacity bytebuffer))))
 
-(defn read [key]
-  (let [loc (.get keys key)]
-    (with-open [cask (cask-file (clojure.core/get loc 3))]
-      (.read cask (clojure.core/get loc 0) (clojure.core/get loc 1)))))
+(defonce key-table (ConcurrentHashMap.))
 
-(defn select-value [blob]
-  ;; TODO:
-  )
-
-(defn byte-buffer [time value-size value key-size key]
-  ;; TODO:
-  )
-
-(defprotocol Bytes
-  (get-bytes [thing]))
+(defn read-fn [vtable]
+  (with-open [cask (RandomAccessFile. (:file-name vtable) "rw")]
+    (.seek cask (:pos vtable))
+    (let [time (.readLong cask)
+          value-size (.readLong cask)
+          value (byte-array value-size)
+          key-size (.readLong cask)
+          key (byte-array key-size)]
+      (.read cask value)
+      (.read cask key)
+      value)))
 
 (def cask
      (reify
@@ -58,8 +79,8 @@
       (valAt [this key] (.valAt this key nil))
       (valAt [this key default]
              (let [key (String. key "utf8")]
-               (if (contains? keys key)
-                 (select-value (read key))
+               (if (.get key-table key)
+                 (read-fn (.get key-table key))
                  default)))
       Associative
       (containsKey [this key]
@@ -71,9 +92,13 @@
         (let [key-size (count key)
               value-size (count value)
               time (System/nanoTime)
-              entry (byte-buffer time value-size value key-size key)]
-          (.put keys (String. key "utf8") entry))
+              entry (doto (ByteBuffer/allocate (+ 8 8 value-size 8 key-size))
+                      (.putLong 0 time)
+                      (.putLong 8 value-size)
+                      (.putLong 16 key-size)
+                      (.position 24)
+                      (.put value)
+                      (.put key)
+                      .flip)]
+          (.put key-table (String. key "utf8") (write entry)))
         this)))
-
-;; table pos | len | uuid | val size
-;; (.put keys key (.write bytebuffer))
