93a3967e284bc4727506a981480c8bef74ab2b76
Kevin Downey (hiredman) <redchin@gmail.com>
2011-06-20 13:09:38 -0700

    newer clojure version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject amontillado "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
