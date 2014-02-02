4753d49879bb211e78cff2a78aa4cbbc60dca8b7
Kevin Downey <redchin@gmail.com>
2014-02-01 20:12:25 -0800

    do it better

--- src/amontillado/cask.clj
+++ src/amontillado/cask.clj
@@ -6,7 +6,9 @@
                     File)
            (java.nio.channels FileChannel)))
 
-(defn crc [^ByteBuffer bb]
+(defn crc
+  "calculate the crc of a byte buffer"
+  [^ByteBuffer bb]
   (.getValue
    (doto (CRC32.)
      (.update (.array bb)))))
@@ -28,7 +30,23 @@
       (.position (+ 32 (count k)))
       (.put v))))
 
-(defn ^ByteBuffer file-record [key value]
+(defn ^ByteBuffer file-record
+  "given a key (bytes) and value (bytes) generate a ByteBuffer for being written
+   to a file. file-record format is:
+
+  crc -- crc of the rest of the record, calculated on the entire
+  record with the crc field as zeros, 8 bytes, a long
+
+  timestamp -- nanoseconds, long
+
+  key size -- byte count in key, long
+
+  value size -- byte count in value, long
+
+  key -- sized as in key size
+
+  value -- sized as in value size"
+  [key value]
   (let [bb (file-record-no-crc (System/nanoTime) key value)
         c (crc bb)]
     (.putLong bb 0 c)
@@ -58,6 +76,9 @@
     bb))
 
 (defn key-dir-entry
+  "a key dir entry is what is kept in memory for each item a long with
+  the key, it has information for finding the value of a given key on
+  disk. it is an array of 4 longs"
   [^long file-id ^long value-size ^long value-position ^long tstamp]
   (let [la (long-array 4)]
     (aset la 0 file-id)
@@ -66,7 +87,12 @@
     (aset la 3 tstamp)
     la))
 
-(defn allocate [^FileChannel fc ^long n]
+(defn allocate
+  "given a file channel, lock on the channel them bump the channel's
+  position by n
+
+  used to pre-allocate space to write in files"
+  [^FileChannel fc ^long n]
   (locking fc
     (let [p (.position fc)]
       (.position fc (+ p n))
@@ -74,7 +100,9 @@
 
 (defrecord CaskFile [id channel raf])
 
-(defn write-to-cask [cask-file key value]
+(defn write-to-cask
+  "write a given key and value to a given file"
+  [cask-file key value]
   (let [^FileChannel fc (:channel cask-file)
         file-id (:id cask-file)
         r (file-record key value)
@@ -89,9 +117,9 @@
 (defn write-tombstone-to-cask [cask-file key]
   (let [^FileChannel fc (:channel cask-file)
         file-id (:id cask-file)
-        r (file-tombstone-record key)]
-    (while (not (zero? (.remaining r)))
-      (.write fc r))
+        r (file-tombstone-record key)
+        pos (allocate fc (.capacity r))]
+    (.write fc r pos)
     (.force fc true)
     nil))
 
@@ -104,16 +132,18 @@
   (let [f (:files cf)]
     (nth f (dec (count f)))))
 
-(defn cask-file [index directory]
-  (let [raf (RandomAccessFile.
-             (io/file directory
-                      (.replace (format "%32s"
-                                        (Long/toHexString index))
-                                " "
-                                "0")) "rw")]
-    (->CaskFile index (.getChannel raf) raf)))
+(defn ^File id-to-file [directory id]
+  (io/file directory (.replace (format "%32s" (Long/toHexString id)) " " "0")))
 
-(defn check-current-file [cf]
+(defn cask-file
+  "given a file-id and a directory, returns a CaskFile with that id"
+  [file-id directory]
+  (let [raf (RandomAccessFile. (id-to-file directory file-id) "rw")]
+    (->CaskFile file-id (.getChannel raf) raf)))
+
+(defn check-current-file
+  "if the latest file is to large, push a new file"
+  [cf]
   {:pre [(:files cf) (:directory cf) (:limit cf)]}
   (if (empty? (:files cf))
     (->CaskFiles [(cask-file 0 (:directory cf))]
@@ -131,17 +161,26 @@
 (deftype BitCask [dict files]
   java.io.Closeable
   (close [_]
-    (doseq [{:keys [^FileChannel channel ^RandomAccessFile raf]} (:files @files)]
+    (doseq [{:keys [^FileChannel channel
+                    ^RandomAccessFile raf]} (:files @files)]
       (.close channel)
       (.close raf))))
 
-(defn new-bitcask [directory & [limit]]
+(defn new-bitcask
+  "start a new bitcask with the given limit"
+  [directory & [limit]]
   (let [directory (io/file directory)]
-    (assert (.mkdirs directory))
+    (.mkdirs directory)
+    (assert (.exists directory))
+    (assert (.isDirectory directory))
+    (assert (empty? (.listFiles directory)))
     (->BitCask (atom {})
                (atom (new-cask-files directory (or limit (* 1024 1024 200)))))))
 
-(defn write-key [^BitCask bc key value]
+(defn write-key
+  "given a key, value, and bitcask, and an entry to the bitcask
+  mapping the key to the value"
+  [^BitCask bc key value]
   (let [cask-files (.-files bc)
         _ (swap! cask-files check-current-file)
         cf (current-file @cask-files)
@@ -149,7 +188,10 @@
         _ (swap! (.-dict bc) assoc (ByteBuffer/wrap key) kr)]
     nil))
 
-(defn delete-key [^BitCask bc key]
+(defn delete-key
+  "removes the given key (byte array) from the in memory map and
+  writes a tombstone to disk"
+  [^BitCask bc key]
   (let [cask-files (.-files bc)
         _ (swap! cask-files check-current-file)
         cf (current-file @cask-files)
@@ -157,7 +199,7 @@
         _ (swap! (.-dict bc) dissoc (ByteBuffer/wrap key))]
     nil))
 
-;; TODO: check crc
+;; TODO: maybe check crc
 (defn read-key [^BitCask bc key]
   (let [key (ByteBuffer/wrap key)]
     (when-let [^longs kr (get @(.-dict bc) key)]
@@ -166,14 +208,13 @@
             pos (+ (aget kr 2) 32 (.capacity key))
             file (nth (:files @(.-files bc)) file-id)
             ^FileChannel fc (:channel file)]
-        ;; TODO: this should not lock
-        (locking fc
-          (.position fc pos)
-          (while (not (zero? (.remaining bb)))
-            (.read fc bb)))
+        (let [bytes-read (.read fc bb pos)]
+          (assert (= bytes-read (.capacity bb))))
         (.array bb)))))
 
-(defn entry-seq [cask-file]
+;; TODO: replace with reducer
+(defn entry-seq
+  [cask-file]
   (let [^RandomAccessFile c (:raf cask-file)]
     ((fn continue [offset]
        (when (< offset (.length c))
@@ -210,11 +251,43 @@
                  (continue (+ offset ks (* 8 5))))))))))
      0)))
 
-(defn open-bitcask [directory & [limit]]
+(defn renumber-files
+  "if dead, no longer needed, files are deleted, the existing files
+  will need to be renumbered, this function does that"
+  [directory files]
+  (doall
+   (map-indexed
+    (fn [i {:keys [id ^FileChannel channel ^RandomAccessFile raf] :as t}]
+      (if (= i id)
+        t
+        (let [from-file (id-to-file directory id)
+              to-file (id-to-file directory i)]
+          (.close channel)
+          (.close raf)
+          (assert (-> from-file (.renameTo to-file)))
+          (let [raf (RandomAccessFile. to-file "rw")]
+            (->CaskFile i (.getChannel raf) raf)))))
+    files)))
+
+(defn dead-files
+  "given a bitcask return a seq of files that are not reference by the
+  in memory map"
+  [^BitCask bc]
+  (let [live-files (set (for [^longs v (vals @(.-dict bc))] (aget v 0)))]
+    (for [f (:files @(.-files bc))
+          :when (not (contains? live-files (:id f)))]
+      (io/file (:directory @(.-files bc))
+               (.replace (format "%32s" (Long/toHexString (:id f))) " " "0")))))
+
+(defn open-bitcask
+  "open a new or existing bitcask
+
+  takes the directory to write the cask files to, and a limit in bytes
+  to split the files at"
+  [directory & [limit]]
   (let [directory (io/file directory)]
     (if (empty? (.listFiles directory))
       (new-bitcask directory limit)
-      ;; TODO: renumber files
       (let [files (for [^File f (file-seq directory)
                         :when (not (.isDirectory f))
                         :let [raf (RandomAccessFile. f "rw")]]
@@ -222,12 +295,22 @@
                      (Long/parseLong (.getName f) 16)
                      (.getChannel raf)
                      raf))
-            files (vec (sort-by :id files))
+            files (vec (renumber-files directory (sort-by :id files)))
+            ;; TODO: combine as a reduce
             dict (into {} (mapcat entry-seq files))
             dict (into {} (for [[k v] dict
                                 :when (not= v ::tombstone)]
-                            [k v]))]
-        (->BitCask (atom dict)
-                   (atom (assoc (new-cask-files directory
-                                                (or limit (* 1024 1024 200)))
-                           :files files)))))))
+                            [k v]))
+            bc (->BitCask (atom dict)
+                          (atom (assoc (new-cask-files
+                                        directory
+                                        (or limit (* 1024 1024 200)))
+                                  :files files)))]
+        (doseq [^File f (dead-files bc)]
+          (.delete f))
+        bc))))
+
+(defn cask-keys
+  "return the keys of this bitcask"
+  [^BitCask bc]
+  (map #(.array ^ByteBuffer %) (keys @(.dict bc))))
