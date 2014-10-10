3858242bf2153cde3a57bd6959cf1696447b6c15
Kevin Downey <redchin@gmail.com>
2014-10-10 11:42:26 -0700

    0.1.0

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject amontillado "1.0.0-SNAPSHOT"
+(defproject amontillado "0.1.0"
   :description "bitcask inspired storage for clojure"
-  :dependencies [[org.clojure/clojure "1.6.0-master-20140201.010403-26"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
